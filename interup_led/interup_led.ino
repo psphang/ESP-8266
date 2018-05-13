c70239e6f697746aca7bcc5cb3de4f5c0be11b98
psphang <phangpohseng@gmail.com>
2018-05-13 23:13:09 +0800

    home

--- interup_led/interup_led.ino
+++ interup_led/interup_led.ino
@@ -1,5 +1,18 @@
-//#include "AudioDriver.h"
 
+
+//#include "AudioDriver.h"
+//mcunode pio to real esp pio
+#define D0 16
+#define D1 5 // I2C Bus SCL (clock)
+#define D2 4 // I2C Bus SDA (data)
+#define D3 0
+#define D4 2 // Same as "LED_BUILTIN", but inverted logic
+#define D5 14 // SPI Bus SCK (clock)
+#define D6 12 // SPI Bus MISO 
+#define D7 13 // SPI Bus MOSI
+#define D8 15 // SPI Bus SS (CS)
+#define D9 3 // RX0 (Serial console)
+#define D10 1 // TX0 (Serial console)
 volatile int toggle;
 int onoff;
 void inline handler (void){
@@ -9,13 +22,15 @@ void inline handler (void){
   }else{
    toggle=1;
   }  
-  digitalWrite(BUILTIN_LED, toggle);
+ // digitalWrite(BUILTIN_LED, toggle);
+    digitalWrite(2, toggle);
   timer0_write(ESP.getCycleCount() + 41660000);
   
 }
  
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);
+ // pinMode(BUILTIN_LED, OUTPUT);
+ pinMode(2, OUTPUT);
 
   noInterrupts();
   timer0_isr_init();
